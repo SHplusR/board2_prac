7280d9f594770c3a78520e5c1d2cd4583c6bd813
SHplusR <devyousim@gmail.com>
2023-05-31 23:22:11 +0900

    sort처리, 검색처리 UI 추가

--- src/main/java/com/example/board_2/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/com/example/board_2/repository/search/SearchBoardRepositoryImpl.java
@@ -6,14 +6,20 @@ import com.example.board_2.entity.QMember;
 import com.example.board_2.entity.QReply;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
@@ -87,9 +93,29 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
             booleanBuilder.and(conditionBuilder);
         }
         tuple.where(booleanBuilder);
+        Sort sort = pageable.getSort();
+
+        sort.stream().forEach(order ->{
+            Order direction = order.isAscending()? Order.ASC : Order.DESC;
+            String prop = order.getProperty();
+
+            PathBuilder orderByExpression = new PathBuilder(Board.class,"board");
+            tuple.orderBy(new OrderSpecifier(direction,orderByExpression.get(prop)));
+        });
         tuple.groupBy(board);
+
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
+
         List<Tuple> result = tuple.fetch();
+
         log.info(result);
-        return null;
+
+        long count = tuple.fetchCount();
+
+        log.info("Count : " + count );
+
+        return new PageImpl<Object[]>(
+                result.stream().map(t -> t.toArray()).collect(Collectors.toList()),pageable,count);
     }
 }
--- src/test/java/com/example/board_2/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/board_2/repository/BoardRepositoryTests.java
@@ -90,8 +90,9 @@ public class BoardRepositoryTests {
 
     @Test
     public void testSearchPage(){
-        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending().and(Sort.by("title").ascending()));
         Page<Object[]> result = boardRepository.searchPage("t","1",pageable);
     }
+
 
 }
