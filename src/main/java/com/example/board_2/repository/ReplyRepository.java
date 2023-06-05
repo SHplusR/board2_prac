f67ece6cde1569896ab709592dc181e0e6729983
SHplusR <devyousim@gmail.com>
2023-06-05 22:47:38 +0900

    replyrepo수정. Board객체를 param으로 받고 해당 Board의 댓글들(Replies)를 댓글번호(Rno)에 따라 가져오는 query method(쿼리 메소드) 추가.

--- src/main/java/com/example/board_2/repository/ReplyRepository.java
+++ src/main/java/com/example/board_2/repository/ReplyRepository.java
@@ -1,13 +1,19 @@
 package com.example.board_2.repository;
 
+import com.example.board_2.entity.Board;
 import com.example.board_2.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply,Long> {
 
     @Modifying
     @Query("delete from Reply r where r.board.bno = :bno")
     void deleteByBno(Long bno);
+
+    //Board객체를 param으로 받고, 해당 Board의 댓글들(Replies)를 댓글번호(Rno)에 따라 가져온다.
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
--- src/test/java/com/example/board_2/repository/ReplyRepositoryTests.java
+++ src/test/java/com/example/board_2/repository/ReplyRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -40,5 +41,11 @@ public class ReplyRepositoryTests {
 
         System.out.println(reply);
         System.out.println(reply.getBoard());
+    }
+
+    @Test
+    public void testListByBoard(){
+        List<Reply> replyList = replyRepository.getRepliesByBoardOrderByRno(Board.builder().bno(96L).build());
+        replyList.forEach(reply -> System.out.println(reply));
     }
 }
